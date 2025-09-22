8ce84a4a108429d20adc9681d1793c6dd2650617
taesamja <taesamja12345@gmail.com>
2025-09-22 20:28:51 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -708,13 +708,13 @@ with st.sidebar:
                   교사01: <code>teacher01 / 1111</code><br>
                   교사02: <code>teacher02 / 1111</code><br>
 
-                  <hr style="border:0; border-top:1px solid rgba(37,99,235,.3); margin:8px 0;">
+                  <hr style="border:0; border-top:1px solid rgba(37,99,235,.3); margin:4px 0;">
                   
                   학생01: <code>student01 / 1111</code><br>
                   학생02: <code>student02 / 1111</code><br>
                   학생03: <code>student03 / 1111</code><br>
                   학생04: <code>student04 / 1111</code><br>
-                  학생05: <code>student05 / 1111</code>
+                  학생05: <code>student05 / 1111</code> 
                 </div>
                 """, unsafe_allow_html=True)
             ############### 테스트 계정 표시 부분 끝 ############################
